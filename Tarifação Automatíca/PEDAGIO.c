a5927a4ce1d7e23f7443e3bed43b94595c5b065b
William Vernaschi <53841356+WilliamVernaschi@users.noreply.github.com>
2020-03-31 18:58:47 -0300

    Add files via upload

---
+++ Tarifação Automatíca/PEDAGIO.c
@@ -0,0 +1,491 @@
+//----BIBLIOTECAS ---- //
+#include <p18f4550.h>
+#include <delays.h>
+#include "lcd.h"
+#include <timers.h>
+
+//--------------------//
+
+//----- CONFIGURA��ES MICRO CONTROLADOR ----//
+#pragma config	FOSC	=	HS
+#pragma config	PWRT	=	ON
+#pragma config	PBADEN	=	OFF
+#pragma config	WDT	=		OFF
+#pragma config	LVP	=		OFF
+//------------------------------------------//
+	
+//----------------- DEFINES ----------------//
+#define LED_VERDE 			PORTDbits.RD1
+#define LED_VERMELHO 		PORTDbits.RD0
+#define CONTROL 			PORTCbits.RC2
+#define SENSOR_SAIDA		PORTBbits.RB5
+#define MOEDEIRO_SINAL		PORTBbits.RB0
+#define ACIONA_MANUAL		PORTAbits.RA4
+//-----------------------------------------//
+
+//declara��o das constantes
+#define ON 					1	
+#define OFF 				0
+#define CONST_T_TIMER0 (65536-416)
+#define CONST_T_TIMER1 (65536-416)
+const char ValorPago = 				1;
+const char Ang_Neg90 =				10;
+const char Ang_Neg60 =				11;
+const char Ang_Neg30 =				13;
+const char Ang_0 = 					15;
+const char Ang_30 =					16;
+const char Ang_60 =					18;
+const char Ang_90 =					20;
+
+
+
+//=====================================================================================
+//				DECLARA��O DAS VARI�VEIS
+//=====================================================================================
+// 1bit
+
+struct
+{
+	unsigned char f_00:1;
+	unsigned char f_01:1;
+	unsigned char f_02:1;
+	unsigned char f_03:1;
+	unsigned char f_04:1;
+	unsigned char f_05:1;
+	unsigned char f_06:1;
+	unsigned char f_07:1;	
+	unsigned char f_08:1;
+}Flags;
+
+#define f_sensor 			Flags.f_00
+#define f_tempo	 			Flags.f_01
+#define f_a		 			Flags.f_02
+#define f_b		 			Flags.f_03
+#define f_c		 			Flags.f_04
+#define f_medindo 			Flags.f_05
+#define f_medindoH 			Flags.f_06
+#define f_medindoL 			Flags.f_07
+#define f_contagem_feita	Flags.f_08
+
+// 8bits
+unsigned char	c_teste	= 0x00;
+unsigned char	tecla	= 0x00;
+unsigned char	dado	= 0x00;
+unsigned char	dado1	= 0x00;
+unsigned char	cont_250ms	= 0x00;
+unsigned char	cont_100ms	= 0x00;
+unsigned char pulso = 			180;
+unsigned char AcionaServo = 	0;
+
+//16 bits
+unsigned int	c_teste1 = 0x00;
+unsigned int	in_ad = 0x00;
+unsigned int i = 				0;
+unsigned int ContadorPulsos = 	0;
+unsigned int ContagemReal = 	0;
+unsigned int ContagemCent = 	0;
+unsigned int cont_tempo = 	0;
+
+
+
+//=====================================================================================
+//				DECLARA��O DAS ROTINAS
+//=====================================================================================
+void Int_Alta (void);
+void Trata_Alta (void);
+void Trata_Timer0(void);
+void Trata_Timer1(void);
+void MostraValorLCD();
+void CentavosParaReal();
+void PosicionaServo (float Angle);
+void conta_pulso (void);
+
+
+//=====================================================================================
+//					Verifica��o das INTERRUP��ES
+//=====================================================================================
+#pragma code Alta = 0x08	//Como se fosse o ORG0000h do ASSEBLER
+
+void Int_Alta (void)		//J� salto para o endere�o onde tem mais espa�o
+{
+	Trata_Alta ();			//Vou para rotina pois aqui n�o tem espa�o
+}
+#pragma code				//para finalizar o pragma do ORG000h
+#pragma interrupt Trata_Alta//para indicar ao compilador que a prioridade da interrup��o � alta
+///////////////////////////
+
+///////////////////////////
+
+
+void Trata_Alta (void)
+{	
+	if (INTCONbits.TMR0IF == 1)
+	{
+		Trata_Timer0();	
+	}
+	if (PIR1bits.TMR1IF == 1)
+	{
+		Trata_Timer1();	
+	}	
+
+}
+
+//=====================================================================================
+//					Tratamento das INTERRUP��ES
+//=====================================================================================
+void Trata_Timer0(void)
+{
+	WriteTimer0(CONST_T_TIMER0);
+	INTCONbits.TMR0IF = 0;
+	/*
+	++cont_tempo_infra;
+	*/
+	
+}
+
+void Trata_Timer1(void)
+{
+	WriteTimer1(CONST_T_TIMER1);
+	PIR1bits.TMR1IF = 0;
+	
+	//Escreva aqui sua interrup��o
+	
+	++cont_tempo;
+	
+		
+	/*
+		++cont_100ms;
+		if (cont_100ms == 100)
+		{
+			cont_100ms=0;
+			f_c=~f_c;
+		}
+	*/
+}
+
+
+// -------------- FUN��ES --------------- //
+void PosicionaServo (float Angle) {		// Fun��o que posiciona o servo e toma o �ngulo como par�metro.
+	CONTROL = ON;
+	Delay100TCYx(Angle);
+	CONTROL = OFF;
+	Delay100TCYx(200 - Angle);
+	}	
+
+
+
+void CentavosParaReal() {		// Fun��o que converte 100 centavos em 1 real.
+    if (ContagemCent == 100) {
+        ContagemReal = ContagemReal + 1;
+        ContagemCent = 0.0;
+        return;
+    }
+}
+
+void MostraValorLCD() {		//Fun��o que mostra no display LCD o valor que est� foi inserido no moedeiro.
+	LimpaLCD();
+	StringLCD("INSERIDOS R$");
+	PosicionaLCD(1, 13);
+	EscreveLCD(ContagemReal + 0x30);
+	PosicionaLCD(1, 14);
+	StringLCD(",");
+	PosicionaLCD(1, 15);
+	if (ContagemCent == 0) {
+		StringLCD("00");
+	} else if (ContagemCent == 5) {
+		StringLCD("05");
+	} else {
+		NumeroLCD(ContagemCent);
+	}
+	Delay1KTCYx(250);
+	return;
+}
+
+// -------------------------------------- //
+
+void conta_pulso (void)
+{
+		if(f_medindo==0)
+		{
+			
+			if (MOEDEIRO_SINAL == 1)
+			
+			{
+				WriteTimer1(CONST_T_TIMER1);
+				T1CONbits.TMR1ON = 1;    //bit que liga e desliga o timer
+				f_medindo=1;
+				f_medindoH=1;
+			}
+			
+		}
+		if(f_medindoH==1)
+		{
+			if (MOEDEIRO_SINAL == 0)
+			{
+				T1CONbits.TMR1ON = 0;    //bit que liga e desliga o timer
+				f_medindoH=0;
+				if ((60<cont_tempo)&&(cont_tempo<80))
+				{
+					++ContadorPulsos;
+					WriteTimer1(CONST_T_TIMER1);	
+					T1CONbits.TMR1ON = 1;    //bit que liga e desliga o timer
+					f_medindoL=1;
+				}	
+				else
+				{
+					f_medindo=0;
+				}						
+				cont_tempo=0;
+			}
+		}
+		if(f_medindoL==1)
+		{
+			if (MOEDEIRO_SINAL == 1)
+			{
+				T1CONbits.TMR1ON = 0;    //bit que liga e desliga o timer
+				f_medindoL=0;
+				if ((90<cont_tempo)&&(cont_tempo<110))
+				{
+					WriteTimer1(CONST_T_TIMER1);	
+					T1CONbits.TMR1ON = 1;    //bit que liga e desliga o timer
+					f_medindoH=1;
+				}	
+				else
+				{
+					f_medindo=0;
+				}						
+				cont_tempo=0;
+			}
+			else if (300<cont_tempo)
+			{	
+				T1CONbits.TMR1ON = 0;    //bit que liga e desliga o timer	
+				f_medindoL=0;		
+				f_medindo=0;
+				f_contagem_feita=1;
+				cont_tempo=0;
+				/*
+				LimpaLCD();
+				PosicionaLCD(1, 1);
+				NumeroLCD(ContadorPulsos);
+				*/
+				
+			}	
+					
+		}		
+			
+}	
+	
+
+
+
+
+// ------- FUN��O MAIN --------- //
+void main() {
+
+    // ENTRADAS E SA�DAS DO MICRO //
+    //AD Essa configura��o de AD � comum para todos os pinos de AD
+	ADCON1 = 0b00001111;
+	TRISB = 0b11111111;
+	TRISC = 0b00000000;
+	UCONbits.USBEN = 0;		//Configura��o necess�ria para deixar os pinos RC4 e RC5
+	UCFGbits.UTRDIS = 1;	//como entradas digitais, desabilitando a USB.
+	TRISD = 0b00000000;
+	TRISE = 0b00001111;
+	PORTB = 0b00000000;
+	PORTC = 0x00;
+	PORTD = 0xFF;
+	PORTA = 0x00;
+	PORTE = 0b00000000;
+	//T0CON = 0b11011001;
+    // -------------------------- //
+    
+//********************************************************************
+//Configura��o dos Timers
+//********************************************************************
+//Tempo do estouro do timer = t cliclo de m�quina * (carga do registrador)*prescaler
+//no caso, (carga do registrador)=65536-500 poi s� 16 bits
+//Timer0//
+//OpenTimer0(TIMER_INT_ON&T0_16BIT&T0_SOURCE_INT&T0_EDGE_FALL&T0_PS_1_2);
+//WriteTimer0(CONST_T_TIMER0);
+//T0CONbits.TMR0ON = 0;    //bit que liga e desliga o timer
+
+//Timer1//
+OpenTimer1(TIMER_INT_ON&T1_16BIT_RW&T1_SOURCE_INT&T1_PS_1_2&T1_OSC1EN_OFF&T1_SYNC_EXT_OFF);
+WriteTimer1(CONST_T_TIMER1);
+T1CONbits.TMR1ON = 0;    //bit que liga e desliga o timer
+
+//Timer2//    
+
+//Timer3//
+    
+    
+    
+//********************************************************************
+//Configura��o das Interrup��es
+//********************************************************************
+    
+    RCONbits.IPEN = 0;		//Definindo para o PIC que todas as interrup��oe s�o de alta prioridade
+    INTCONbits.GIE = 1;		//habilita a interrup��o global
+    INTCONbits.PEIE = 1;	//habilita as interrup��es de perif�rico
+    
+    
+    
+    f_medindo			=0;
+	f_medindoH			=0;	
+	f_medindoL			=0;
+	f_contagem_feita	=0;
+    
+    
+    
+    
+    
+	LED_VERMELHO = ON;
+	InicializaLCD();
+	LimpaLCD();
+	PosicionaLCD(1, 1);
+	StringLCD("BEM VINDO!      ");
+	PosicionaLCD(2, 1);
+	StringLCD("VALOR: R$ ,00   ");
+	PosicionaLCD(2, 10);
+	EscreveLCD(ValorPago + 0x30);
+	for (i = 0; i < 150; i = i + 1) {
+	PosicionaServo (Ang_Neg30);
+	}
+	LED_VERMELHO = ON;
+	LED_VERDE = OFF;	
+	
+	
+	
+	
+	
+//LOOP INFINITO	
+
+	while (ON) 
+	{
+		if (f_contagem_feita==0)
+		{
+			conta_pulso();
+		}
+		
+		
+		if(f_contagem_feita==1)
+		{
+		
+		
+			switch (ContadorPulsos) {
+			case 1: // CASO RECONHE�A A MOEDA DE 1 REAL
+				if (ContagemReal < ValorPago){
+				ContagemReal += 1;
+				MostraValorLCD();
+				}    
+				break;
+			case 2: // CASO RECONHE�A A MOEDA DE 50 CENTAVOS GROSSA
+				if (ContagemReal < ValorPago) {
+					for (i = 0; i < 50; i = i + 1) {
+						ContagemCent = ContagemCent + 1;
+						CentavosParaReal();
+						}
+						MostraValorLCD();
+	            	}
+				break;
+			case 3: // CASO RECONHE�A A MOEDA DE 50 CENTAVOS FINA
+				if (ContagemReal < ValorPago) {
+					for (i = 0; i < 50; i = i + 1) {
+						ContagemCent = ContagemCent + 1;
+						CentavosParaReal();
+						}
+						MostraValorLCD();
+	                }
+	            
+				break;
+			case 4: // CASO RECONHE�A A MOEDA DE 25 CENTAVOS
+					if (ContagemReal < ValorPago) {
+						for (i = 0; i < 25; i = i + 1) {
+							ContagemCent = ContagemCent + 1;
+							CentavosParaReal();
+						}
+						MostraValorLCD();
+					}
+				
+				break;
+			case 5: // CASO RECONHE�A A MOEDA DE 10 CENTAVOS
+					if (ContagemReal < ValorPago) {
+						for (i = 0; i < 10; i = i + 1) {
+							ContagemCent = ContagemCent + 1;
+							CentavosParaReal();
+						}
+						MostraValorLCD();
+					}
+				
+				break;
+			case 6: // CASO RECONHE�A A MOEDA DE 5 CENTAVOS
+					if (ContagemReal < ValorPago) {
+						for (i = 0; i < 5; i = i + 1) {
+							ContagemCent = ContagemCent + 1;
+							CentavosParaReal();
+						}
+						MostraValorLCD();
+				}
+				break;
+			}
+		
+			f_contagem_feita=0;
+			ContadorPulsos=0;
+		}	
+		
+		
+		
+		
+		if (ContagemReal >= ValorPago) 
+		{ // SE O VALOR INSERIDO FOR MAIOR OU IGUAL O VALOR NECESS�RIO
+			LimpaLCD();
+			PosicionaLCD(1, 5);
+			StringLCD("VALOR PAGO");
+			PosicionaLCD(2, 5);
+			StringLCD("BOA VIAGEM!");
+			Delay1KTCYx(250);
+			LED_VERDE = 1;
+			LED_VERMELHO = 0;
+			AcionaServo = 1;
+			if (AcionaServo == 1)
+			{
+				AcionaServo = 0;
+				ContagemReal=0;
+				ContagemCent=0;
+				
+				for (i = 0; i < 250; i = i + 1) 
+				{
+					PosicionaServo (Ang_90);
+				}
+			
+			if (SENSOR_SAIDA == 0){
+				for (i = 0; i < 50; i = i + 1) 
+				{
+					PosicionaServo (Ang_Neg30);
+				}	
+					LED_VERMELHO = ON;
+					LED_VERDE = OFF;
+					InicializaLCD();
+					LimpaLCD();
+					PosicionaLCD(1, 1);
+					StringLCD("BEM VINDO!      ");
+					PosicionaLCD(2, 1);
+					StringLCD("VALOR: R$ ,00   ");
+					PosicionaLCD(2, 10);
+					EscreveLCD(ValorPago + 0x30);	
+				
+		}
+				
+				
+			
+					
+			}
+		}
+		
+		
+		
+		
+			
+	}//FECHA WHILE
+} // END MAIN 
+    
---
+++ Tarifação Automatíca/lcd.h
@@ -0,0 +1,239 @@
+// =======================================================================
+// Escola SENAI "A. Jacob Lafer"
+//
+// Curso:		Técnico de Eletroeletrônica
+// Disciplina:	Sistemas Eletrônicos Digitais
+//
+// Biblioteca:	Controle de Display LCD 2x16 e 4x16 no modo 4 bits
+//
+// Descrição:	
+//
+// Ano:			2018
+// Revisão:		D
+//
+// Autor:		Prof. Cleisson
+// =======================================================================
+
+#ifndef _LCD_H_
+#define _LCD_H_
+
+//=====================================================================================
+//	BIBLIOTECAS
+//=====================================================================================
+#include <delays.h>
+
+//=====================================================================================
+//	DECLARAÇÃO DAS SUBROTINAS
+//=====================================================================================
+void InicializaLCD	(void);
+void ComandoLCD		(unsigned char comando);
+void EscreveLCD		(unsigned char dado);
+void StringLCD		(const rom unsigned char *dado);
+void NumeroLCD		(unsigned int numero);
+void PosicionaLCD	(unsigned char linha, unsigned char coluna);
+void LimpaLCD		(void);
+
+//=====================================================================================
+//	DEFINIÇÕES DE HARDWARE
+//=====================================================================================
+#define RS_LCD		PORTDbits.RD2
+#define EN_LCD		PORTDbits.RD3
+#define RW_LCD		PORTEbits.RE2
+#define D4_LCD		PORTDbits.RD4
+#define D5_LCD		PORTDbits.RD5
+#define D6_LCD		PORTDbits.RD6
+#define D7_LCD		PORTDbits.RD7
+
+#define T_RS_LCD	TRISDbits.RD2
+#define T_EN_LCD	TRISDbits.RD3
+#define T_RW_LCD	TRISEbits.RE2
+#define T_D4_LCD	TRISDbits.RD4
+#define T_D5_LCD	TRISDbits.RD5
+#define T_D6_LCD	TRISDbits.RD6
+#define T_D7_LCD	TRISDbits.RD7
+
+//=====================================================================================
+//	SUBROTINA DE INICIALIZAÇÃO NO MODO 4 BITS DO LCD
+//=====================================================================================
+void InicializaLCD (void)
+{
+	unsigned char ADCON1_bkp;
+	
+	ADCON1_bkp = ADCON1;
+	ADCON1 = (ADCON1 & 0xF0) | 0x0A;
+	
+	T_RS_LCD = 0;
+	T_RW_LCD = 0;
+	T_EN_LCD = 0;
+	T_D4_LCD = 0;
+	T_D5_LCD = 0;
+	T_D6_LCD = 0;
+	T_D7_LCD = 0;
+
+	ComandoLCD (0x33);
+	ComandoLCD (0x32);
+	ComandoLCD (0x28);
+	ComandoLCD (0x28);
+	ComandoLCD (0x0C);
+	ComandoLCD (0x06);
+	ComandoLCD (0x01);
+	
+	ADCON1 = ADCON1_bkp;
+}
+
+//=====================================================================================
+//	SUBROTINA DE CONTROLE DO LCD
+//=====================================================================================
+void ComandoLCD (unsigned char comando)
+{
+	unsigned char ADCON1_bkp;
+	
+	ADCON1_bkp = ADCON1;
+	ADCON1 = (ADCON1 & 0xF0) | 0x0A;
+	
+	RS_LCD = 0;
+	RW_LCD = 0;
+	EN_LCD = 0;
+	
+	D7_LCD = (comando & 0x80) >> 7;
+	D6_LCD = (comando & 0x40) >> 6;
+	D5_LCD = (comando & 0x20) >> 5;
+	D4_LCD = (comando & 0x10) >> 4;
+	EN_LCD = 1;
+	Delay10TCYx(50);
+	EN_LCD = 0;
+	Delay10TCYx(50);
+
+	D7_LCD = (comando & 0x08) >> 3;
+	D6_LCD = (comando & 0x04) >> 2;
+	D5_LCD = (comando & 0x02) >> 1;
+	D4_LCD = (comando & 0x01);
+	EN_LCD = 1;
+	Delay10TCYx(50);
+	EN_LCD = 0;
+	Delay1KTCYx(10);
+	
+	ADCON1 = ADCON1_bkp;
+}
+
+//=====================================================================================
+//	SUBROTINA DE ESCRITA DE CARACTER DO LCD
+//=====================================================================================
+void EscreveLCD (unsigned char dado)
+{
+	unsigned char ADCON1_bkp;
+	
+	ADCON1_bkp = ADCON1;
+	ADCON1 = (ADCON1 & 0xF0) | 0x0A;
+	
+	RS_LCD = 1;
+	RW_LCD = 0;
+	EN_LCD = 0;
+	
+	D7_LCD = (dado & 0x80) >> 7;
+	D6_LCD = (dado & 0x40) >> 6;
+	D5_LCD = (dado & 0x20) >> 5;
+	D4_LCD = (dado & 0x10) >> 4;
+	EN_LCD = 1;
+	Delay10TCYx(50);
+	EN_LCD = 0;
+	Delay10TCYx(50);
+	
+	D7_LCD = (dado & 0x08) >> 3;
+	D6_LCD = (dado & 0x04) >> 2;
+	D5_LCD = (dado & 0x02) >> 1;
+	D4_LCD = (dado & 0x01);
+	EN_LCD = 1;
+	Delay10TCYx(50);
+	EN_LCD = 0;
+	Delay1KTCYx(10);
+	
+	ADCON1 = ADCON1_bkp;
+}
+
+//=====================================================================================
+//	SUBROTINA DE ESCRITA DE TEXTO DO LCD
+//=====================================================================================
+void StringLCD (const rom unsigned char *dado)
+{
+	while (*dado != '\0')
+	{
+		EscreveLCD(*dado);
+		++ dado;
+	}
+}
+
+//=====================================================================================
+//	SUBROTINA DE POSICIONAMENTO DO CURSOR DE ESCRITA DO LCD
+//=====================================================================================
+void PosicionaLCD (unsigned char linha, unsigned char coluna)
+{
+	-- coluna;
+
+	switch (linha)
+	{
+		case 1:
+			ComandoLCD(0x80 + coluna);
+		break;
+
+		case 2:
+			ComandoLCD(0xC0 + coluna);
+		break;
+
+		case 3:
+			ComandoLCD(0x90 + coluna);
+		break;
+
+		case 4:
+			ComandoLCD(0xD0 + coluna);
+		break;
+
+		default:
+		break;	
+	}
+}
+
+//=====================================================================================
+//	SUBROTINA PARA ESCREVER UM NÚMERO DE UMA VARIÁVEL NO LCD
+//=====================================================================================
+void NumeroLCD (unsigned int numero)
+{
+	unsigned char x[5];
+	unsigned char i;
+	unsigned char j;
+	
+	x[4] = numero / 10000;
+	numero = numero % 10000;
+	x[3] = numero / 1000;
+	numero = numero % 1000;
+	x[2] = numero / 100;
+	numero = numero % 100;
+	x[1] = numero / 10;
+	x[0] = numero % 10;
+	
+	for (i = 4; (x[i] == 0)&&(i > 0) ; --i);
+	
+	j = 4 - i;
+	
+	do
+	{
+		EscreveLCD(x[i] + 0x30);
+		--i;
+	} while (i != 0xFF);
+	
+	while (j > 0)
+	{
+		EscreveLCD(' ');
+		--j;
+	}		
+}	
+//=====================================================================================
+//	SUBROTINA PARA APAGAR O CONTEÚDO ESCRITO NO LCD
+//=====================================================================================
+void LimpaLCD (void)
+{
+	ComandoLCD(0x01);
+}
+//=====================================================================================
+
+#endif	// _LCD_H_
